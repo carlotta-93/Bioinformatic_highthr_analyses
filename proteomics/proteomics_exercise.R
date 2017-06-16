34f1995830eb954127be37ee7c6cc637e9411832
Carlotta <carlottapor@gmail.com>
2017-06-16 11:59:59 +0200

    homework 4
    homework 4 part 3

--- proteomics/proteomics_exercise.R
+++ proteomics/proteomics_exercise.R
@@ -5,4 +5,11 @@ filteredproteins <- filteredproteins[filteredproteins$Reverse != "+", ]
 filteredproteins <- filteredproteins[which(filteredproteins$Peptides..seq.>2), ]
 filteredproteins <- filteredproteins[which(filteredproteins$Unique.Peptides..seq.>=2), ]
 
-filteredproteins
+hist(log(filteredproteins$Ratio.H.L.Normalized.CDCH), breaks=50)
+hist(log(filteredproteins$Ratio.H.L.Normalized.CDCL))
+
+plot(log(filteredproteins$Ratio.H.L.Normalized.CDCH), log(filteredproteins$Ratio.H.L.Normalized.CDCL))
+
+filteredproteins$Gene.Names[filteredproteins$Ratio.H.L.Significance.A..CDCH < 0.0001 & !is.na(filteredproteins$Ratio.H.L.Significance.A..CDCH)]
+c <- filteredproteins$Gene.Names[filteredproteins$Ratio.H.L.Significance.A..CDCL < 0.0001 & !is.na(filteredproteins$Ratio.H.L.Significance.A..CDCL)]
+c
